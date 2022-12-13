d8d498ade95b646109307c879cd52c94b3d0a3a6
Jimmy Sommer <jimmy.sommer64@gmail.com>
2022-12-12 20:06:39 -0600

    Recent Build

--- rotmg/src/App.js
+++ rotmg/src/App.js
@@ -1,10 +1,46 @@
 import './App.css';
 import Set from './Components/Set/Set';
 
+const characters = importAll(require.context("./images/Characters",false,/\.(png|jpe?g|svg)$/));
+const items = importAll(require.context("./images/Items",false,/\.(png|jpe?g|svg)$/));
+
+const numSets = 15;
+//const numCols = 3;
+
 function App() {
+  const disp=[];
+  for (let i=1; i<numSets+1; i++){
+    disp.push(
+        <Set 
+        c={characters[String(i)+".png"]}
+        cn={"Character Name"}
+        s1n={"Item 1"}
+        s2n={"Item 2"}
+        s3n={"Item 3"}
+        s4n={"Item 4"}
+        s1={items[String(i*4-3)+".png"]}
+        s2={items[String(i*4-2)+".png"]}
+        s3={items[String(i*4-1)+".png"]}
+        s4={items[String(i*4)+".png"]}
+        />
+    )
+  }
+  const ndisp=[];
+  while(disp.length>0) {
+    ndisp.push(disp.splice(0,3))
+  }
+
+  
   return(
-    <Set/>
-  );
+    ndisp
+    );
+};
+
+function importAll(r) {
+  let images = {};
+  // eslint-disable-next-line
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
 }
 
 export default App;
--- rotmg/src/Components/Set/Set.js
+++ rotmg/src/Components/Set/Set.js
@@ -1,31 +1,34 @@
 import './Set.css'
-const characters = importAll(require.context("../../images/Characters",false,/\.(png|jpe?g|svg)$/));
-const items = importAll(require.context("../../images/Items",false,/\.(png|jpe?g|svg)$/));
 
 function Set(props) {
+
     return (
         <div class="set-container">
-            <h1>Set Name</h1>
+            <h1>{props.cn}</h1>
             <div class="character">
-                <img class="char" src={characters["1.png"]}/>
+                <img class="charimg" src={props.c} alt={""}/>
             </div>
-            <div class="item1"><h1>Item 1</h1><img class="item" src={items["1.png"]}/></div>
-            <div class="item2"><h1>Item 2</h1><img class="item" src={items["2.png"]}/></div>
-            <div class="item3"><h1>Item 3</h1><img class="item" src={items["3.png"]}/></div>
-            <div class="item4"><h1>Item 4</h1><img class="item" src={items["4.png"]}/></div>
+            <div class="item1">
+                <h1 class="itemtext">{props.s1n}</h1>
+                <img class="itemimg" src={props.s1} alt={""}/>
+            </div>
+            <div class="item2">
+                <h1 class="itemtext">{props.s2n}</h1>
+                <img class="itemimg" src={props.s2} alt={""}/>
+            </div>
+            <div class="item3">
+                <h1 class="itemtext">{props.s3n}</h1>
+                <img class="itemimg" src={props.s3} alt={""}/>
+            </div>
+            <div class="item4">
+                <h1 class="itemtext">{props.s4n}</h1>
+                <img class="itemimg" src={props.s4} alt={""}/>
+            </div>
         </div>
     )
 }
 
-function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-
 export default Set;
-
-//import Nej from './src/images/nej.jpg';
 
 //({gameclass}, {example set name}, {item 1 image}, {item 2 image}, {item 3 image}, {item 4 image}, {full set image} )
 //({rogue}, {vampire rogue}, {soulcursed scythe image}, {hallowed hide image}, {vampiric cape image}, {ruthven's rosary image}, {spooky vampire guy image} )
